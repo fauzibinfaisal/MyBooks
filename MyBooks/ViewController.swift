8681d723653c7d294dafd79d88159f1e4f1d32fe
Fauzi <fauzibinfaisal@gmail.com>
2021-01-09 05:49:59 +0700

    Initial Commit

---
+++ MyBooks/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyBooks
+//
+//  Created by Fauzi Fauzi on 09/01/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
